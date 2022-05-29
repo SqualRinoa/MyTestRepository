79d84b0271cea7a32634e61a99767ab5e30a178c
SqualRinoa <kuroneko1985@yandex.ru>
2022-05-29 19:08:44 +0300

    2 - test2.sh - time

--- test2.sh
+++ test2.sh
@@ -1,1 +1,10 @@
 #!/bin/sh
+exec &> /etc/motd
+    #
+    #
+    # Time of day
+    HOUR=$(date +"%H")
+    if [ $HOUR -lt 12  -a $HOUR -ge 0 ]; then TIME="morning"
+    elif [ $HOUR -lt 17 -a $HOUR -ge 12 ]; then TIME="afternoon"
+    else TIME="evening"
+
