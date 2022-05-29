2e83bcb5f09ced7e09faaaad92849590df0c3e3c
SqualRinoa <kuroneko1985@yandex.ru>
2022-05-29 19:10:15 +0300

    3 - test2.sh - uptime

--- test2.sh
+++ test2.sh
@@ -7,4 +7,11 @@ exec &> /etc/motd
     if [ $HOUR -lt 12  -a $HOUR -ge 0 ]; then TIME="morning"
     elif [ $HOUR -lt 17 -a $HOUR -ge 12 ]; then TIME="afternoon"
     else TIME="evening"
-
+    fi
+    #
+    # System uptime
+    uptime=`cat /proc/uptime | cut -f1 -d.`
+    upDays=$((uptime/60/60/24))
+    upHours=$((uptime/60/60%24))
+    upMins=$((uptime/60%60))
+    #
