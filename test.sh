d99b4814e47781defa931923b4c2bba3a8543472
SqualRinoa <kuroneko1985@yandex.ru>
2022-05-29 18:22:30 +0300

    Fourth commit - test.sh-sys+memory

--- test.sh
+++ test.sh
@@ -10,3 +10,32 @@
     else TIME="evening"
     fi
     #
+    # System uptime
+    uptime=`cat /proc/uptime | cut -f1 -d.`
+    upDays=$((uptime/60/60/24))
+    upHours=$((uptime/60/60%24))
+    upMins=$((uptime/60%60))
+    #
+    # System + Memory
+    SYS_LOADS=`cat /proc/loadavg | awk '{print $1}'`
+    MEMORY_USED=`free -b | grep Mem | awk '{print $3/$2 * 100.0}'`
+    SWAP_USED=`free -b | grep Swap | awk '{print $3/$2 * 100.0}'`
+    NUM_PROCS=`ps aux | wc -l`
+    IPADDRESS=`hostname --all-ip-addresses`
+    #
+    echo  "================================================================="
+    echo  " Good $TIME !                              NETOLOY Corp Ltd."
+    echo  "================================================================="
+    echo  " - Hostname      : `hostname -f`"
+    echo  " - IP Address    : $IPADDRESS"
+    echo  " - Release       : $(lsb_release -s -d)[$(cat /etc/debian_version)]"
+    echo  " - Kernel        : `uname -a | awk '{print $1" "$3" "$12}'`"
+    echo  " - Users         : Currently `users | wc -w` user(s) logged on"
+    echo  " - Server Time   : `date`"
+    echo  " - System load   : $SYS_LOADS / $NUM_PROCS processes running"
+    echo  " - Memory used % : $MEMORY_USED"
+    echo $tcLtG " - Swap used %   : $SWAP_USED"
+    echo $tcLtG " - System uptime : $upDays days $upHours hours $upMins minutes"
+    echo $tcDkG "================================================================="
+    echo $tcRESET ""
+    #
